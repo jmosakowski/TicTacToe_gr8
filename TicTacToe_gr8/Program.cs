0aaed629206e82739806fe32134795d1f38ae585
Jacek Mosakowski <Jacek Mosakowski@DESKTOP-RIS73BT>
2022-12-17 11:50:13 +0100

    Added board drawing method

--- TicTacToe_gr8/Program.cs
+++ TicTacToe_gr8/Program.cs
@@ -6,6 +6,7 @@ namespace TicTacToe_gr8
     {
         static void Main(string[] args)
         {
+            // Create two boards - with starting and with current fields
             char[,] startBoard = {
                 { '1', '2', '3' },
                 { '4', '5', '6' },
@@ -17,15 +18,15 @@ namespace TicTacToe_gr8
             bool player1Won = false;
             bool player2Won = false;
             bool nextIsPlayer1 = true; // true - player 1 move, false - player 2 move
-                                       
+
             ////////////////////////////////////////////////////////////////
-                                       
+
             // Loop over rounds
             for (int round = 0; round < gameBoard.Length; round++)
             {
                 Console.Clear();
-                // TODO: draw the board
-                
+                Draw(gameBoard);
+
                 if (nextIsPlayer1)
                 {
                     // TODO: player 1 move
@@ -36,18 +37,30 @@ namespace TicTacToe_gr8
                     // TODO: player 2 move
                     nextIsPlayer1 = true;
                 }
-                
+
                 if (player1Won || player2Won)
                     break;
             }
-            
+
             ////////////////////////////////////////////////////////////////
-            
+
             // End the game
             Console.Clear();
-            // TODO: draw the board
+            Draw(gameBoard);
             Console.Write("Game ended! ");
             // TODO: print who won
+        }
+
+        /******************************************************************/
+
+        static void Draw(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                    Console.Write(board[i, j]);
+                Console.WriteLine();
+            }
         }
     }
 }
